7844dce2e6a07903cd801e4042148971cb071201
hlwprpht <sergbashan@yandex.ru>
2024-10-06 11:47:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,43 +5,40 @@ from urllib.parse import urlparse
 
 
 def shorten_link(token, long_url):
-    url = 'https://api.vk.ru/method/utils.getShortLink'
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    params={"v":5.199, "url":long_url}
+    url = "https://api.vk.ru/method/utils.getShortLink"
+    headers = {"Authorization": f"Bearer {token}"}
+    params = {"v": 5.199, "url": long_url}
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
-    return response.json()['response']['short_url']
+    return response.json()["response"]["short_url"]
 
 
 def count_clicks(token, short_url):
-    url = 'https://api.vk.ru/method/utils.getLinkStats'
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    params={"v":5.199, "key":short_url, "extended":0, "interval":"forever"}
+    url = "https://api.vk.ru/method/utils.getLinkStats"
+    headers = {"Authorization": f"Bearer {token}"}
+    params = {"v": 5.199, "key": short_url, "extended": 0, "interval": "forever"}
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
-    return response.json()['response']
+    return response.json()["response"]
 
 
 def main():
     load_dotenv()
-    token=os.environ['VK_TOKEN']
-    link=input("Введите ссылку: ")
-    parsed_link=urlparse(link)
+    token = os.environ["VK_TOKEN"]
+    link = input("Введите ссылку: ")
+    parsed_link = urlparse(link)
     try:
-        if parsed_link.netloc=="vk.cc":
-            print("Количество кликов по ссылке",count_clicks(token, parsed_link.path[1:]))
+        if parsed_link.netloc == "vk.cc":
+            print(
+                "Количество кликов по ссылке", count_clicks(token, parsed_link.path[1:])
+            )
         else:
-            print('Сокращенная ссылка: ', shorten_link(token, link))
+            print("Сокращенная ссылка: ", shorten_link(token, link))
     except requests.exceptions.HTTPError:
         print("Проверьте вашу ссылку")
 
-        
+
 if __name__ == "__main__":
     main()
-    
 
     
